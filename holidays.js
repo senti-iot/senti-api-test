27c10e3c2baf19750ea7797adfb271cc9e6f946b
Christian Broberg <cb@cb-pro.local>
2018-12-03 18:34:35 +0100

    Testing holidays API ... works locally

---
+++ holidays.js
@@ -0,0 +1,45 @@
+require('dotenv').load()
+const create = require('apisauce').create
+const { encrypt } = require('senti-apicore')
+
+const { ENCRYPTION_KEY, API_LOCAL, API_DEV } = process.env
+
+const apiRoute = '/holidays/v1/2018-01-01/2018-12-31/da'
+const numRetry = 5
+
+const api = create({
+	baseURL: API_LOCAL,
+	timeout: 30000,
+	headers: {
+		'auth': encrypt(ENCRYPTION_KEY)
+	}
+})
+
+const apiCall = async (n) => {
+	let response
+	try {
+		response = await api.get(apiRoute)
+	} catch (error) {
+		if (n === 1) {
+			console.error(error)
+		}
+		response = await apiCall(n - 1)
+	}
+	// check response	
+	if (response.ok && response.status == 200) {
+		console.log('API/holidays:', response.status, Date())
+		return response.data
+	} else {
+		console.log('API/holidays Error:', response.status, Date())
+		return 403
+	}
+}
+
+const result = async () => {
+	let response
+	response = await apiCall(numRetry)
+	console.log(response)
+	return
+}
+
+result()
